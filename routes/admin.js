dff8bf14eddbd6e8654c9c946c91fd4fd814f24a
FehmiCitiloglu <fehmicitiloglu@gmail.com>
2022-10-09 09:52:24 +0300

    edit product page with data

--- routes/admin.js
+++ routes/admin.js
@@ -3,7 +3,7 @@ const path = require('path');
 const express = require('express');
 
 const rootDir = require('../util/path');
-const { getAddProduct, postAddProduct, getProducts } = require('../controllers/admin');
+const { getAddProduct, postAddProduct, getProducts, getEditProduct } = require('../controllers/admin');
 
 const router = express.Router();
 
@@ -17,5 +17,7 @@ router.post('/add-product', postAddProduct);
 
 // /admin/product => GET
 router.get('/products', getProducts);
+
+router.get("/edit-product/:productId", getEditProduct)
 
 module.exports = router
