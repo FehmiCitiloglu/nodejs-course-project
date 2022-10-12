05d85a5ce269644521ad8a299a6e7e3dfd5039d4
FehmiCitiloglu <fehmicitiloglu@gmail.com>
2022-10-12 08:24:14 +0300

    recreate Product model with sequelize

--- models/product.js
+++ models/product.js
@@ -1,35 +1,34 @@
+const Sequelize = require("sequelize")
 
-const db = require("../util/database")
+const sequelize = require("../util/database")
 
-const Cart = require("./cart")
 
-module.exports = class Product {
-    constructor(id, title, imageUrl, description, price) {
-        this.title = title
-        this.id = id
-        this.imageUrl = imageUrl
-        this.description = description
-        this.price = price
+const Product = sequelize.define('product',
+    {
+        id: {
+            type: Sequelize.SMALLINT,
+            autoIncrement: true,
+            allowNull: false,
+            primaryKey: true
+        },
+        title: {
+            type: Sequelize.STRING,
+            allowNull: false,
+        },
+        price: {
+            type: Sequelize.DOUBLE,
+            allowNull: false,
+        },
+        image_url: {
+            type: Sequelize.STRING,
+            allowNull: false,
+        },
+        description: {
+            type: Sequelize.TEXT,
+            allowNull: false,
+        }
     }
+);
 
-    save() {
-        return db.query(
-            {
-                text: "INSERT INTO products(title, price, image_url, description) VALUES ($1, $2, $3, $4) RETURNING *",
-                values: [this.title, this.price, this.imageUrl, this.description]
-            }
-        )
-    }
 
-    static deleteById(id) {
-
-    }
-
-    static fetchAll() {
-        return db.query("SELECT * FROM products")
-    }
-
-    static findById(id) {
-        return db.query("SELECT * FROM products WHERE id=$1", [id])
-    }
-} 
+module.exports = Product
