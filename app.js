ea40dc034987505788613e3ad09015a01d39dc3a
FehmiCitiloglu <fehmicitiloglu@gmail.com>
2022-10-12 09:21:42 +0300

    syncing js definition to database

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 // const expressHandlebar = require("express-handlebars")
 
-const db = require("./util/database")
+const sequelize = require("./util/database")
 
 const app = express();
 
@@ -38,4 +38,13 @@ app.use(shopRoutes);
 
 app.use(get404);
 
-app.listen(3000);
+sequelize
+    .sync()
+    .then(result => {
+        console.log(result)
+        app.listen(3000);
+    })
+    .catch((err) => {
+        console.error(err);
+    })
+
