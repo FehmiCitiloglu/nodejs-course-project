322707ab46ec1e5ee0ba54bd4faf365f35ab0100
FehmiCitiloglu <fehmicitiloglu@gmail.com>
2022-10-12 22:18:20 +0300

    add new product to cart

--- controllers/shop.js
+++ controllers/shop.js
@@ -90,11 +90,42 @@ exports.getCheckout = (req, res, next) => {
 
 exports.postCart = (res, req, next) => {
     const { productId } = res.body
-    Product.findById(productId, (product) => {
-        Cart.addProduct(productId, product.price)
-    })
+    let fetchedCart
+    console.log(req.user);
+    req.user.getCart()
+        .then((cart) => {
+            fetchedCart = cart
+            return cart.getProducts({ where: { id: productId } })
+        }).then((products) => {
+            let product
+            if (products.length > 0) {
+                product = products[0]
+            }
+            let newQuantity = 1
+            if (product) {
+                // ...
+            }
+            return Product.findByPk(productId)
+                .then((product) => {
+                    return fetchedCart.addProduct(product, {
+                        through: { quantity: newQuantity }
+                    })
+                })
+                .catch((err) => console.error(err))
 
-    req.redirect("/cart")
+        })
+        .then(() => {
+            res.redirect("/cart")
+        })
+        .catch((err) => {
+            console.error(err);
+        })
+
+    // Product.findById(productId, (product) => {
+    //     Cart.addProduct(productId, product.price)
+    // })
+
+    // req.redirect("/cart")
 }
 
 exports.postCartDeleteProduct = (req, res, next) => {
