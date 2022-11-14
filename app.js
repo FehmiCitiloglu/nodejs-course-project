8fbd94ef675c08057901fe0e83cf30c62703299b
FehmiCitiloglu <fehmicitiloglu@gmail.com>
2022-11-14 22:42:24 +0300

    using relations in mongoose

--- app.js
+++ app.js
@@ -9,28 +9,27 @@ const app = express();
 app.set("view engine", "ejs");
 app.set("views", "views");
 
-// const User = require("./models/user");
-
+const User = require("./models/user");
 const adminRoutes = require("./routes/admin");
 const shopRoutes = require("./routes/shop");
 const { get404 } = require("./controllers/error");
 
-app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, "public")));
+app.use(bodyParser.urlencoded({ extended: false }));
 
-// app.use((req, res, next) => {
-//   User.findById("6349dc1e9e95fa4c567428c7")
-//     .then((user) => {
-//       res.user = new User(user.name, user.email, user.cart, user._id);
-//       next();
-//     })
-//     .catch((err) => {
-//       console.error(err);
-//     });
-// });
+app.use((req, res, next) => {
+  User.findById("6372901c569cef333180ac1d")
+    .then((user) => {
+      res.user = user;
+      next();
+    })
+    .catch((err) => {
+      console.error(err);
+    });
+});
 
-app.use("/admin", adminRoutes);
 app.use(shopRoutes);
+app.use("/admin", adminRoutes);
 
 app.use(get404);
 
@@ -39,6 +38,19 @@ mongoose
     `mongodb+srv://${process.env.USERNAME}:${process.env.PASSWORD}@cluster0.dxkoo.mongodb.net/shop?retryWrites=true&w=majority`
   )
   .then((result) => {
+    User.findOne().then((user) => {
+      if (!user) {
+        const user = new User({
+          name: "Fehmi",
+          email: "test@test.com",
+          cart: {
+            items: [],
+          },
+        });
+        user.save();
+      }
+    });
+
     app.listen(3000);
     console.info("db connected");
   })
--- controllers/admin.js
+++ controllers/admin.js
@@ -11,7 +11,13 @@ exports.getAddProduct = (req, res, next) => {
 
 exports.postAddProduct = (req, res, next) => {
   const { title, imageUrl, description, price } = req.body;
-  const product = new Product({ title, price, description, imageUrl });
+  const product = new Product({
+    title,
+    price,
+    description,
+    imageUrl,
+    userId: res.user,
+  });
 
   product
     .save()
